1cfd144667e5ca7d36e01e51b011ce939033c371
renzo <molenakram8@gmail.com>
2024-11-09 05:51:55 +0700

    make controller

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const { manusia, pria, wanita , saveData} = require('./function');
+const { manusia, pria, wanita , saveData} = require('./model/function');
 const { manusiaList } = require('./database/db');
 
 const app = express();
--- model/function.js
+++ model/function.js
@@ -1,4 +1,4 @@
-const { manusiaList } = require('./database/db');
+const { manusiaList } = require('../database/db');
 const fs = require('fs');
 
 class manusia {
